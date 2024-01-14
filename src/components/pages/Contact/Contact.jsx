2798d6587c9f547f7759afcf51b9110e3edd3463
marcosalvaradomartinez <mam02@iesemilidarder.com>
2024-01-14 20:14:40 +0100

    Proyecto sin media queries

--- src/components/pages/Contact/Contact.jsx
+++ src/components/pages/Contact/Contact.jsx
@@ -1,38 +1,60 @@
-import React from 'react'
+import React from 'react';
+import emailjs from 'emailjs-com';
+import styles from './Contact.module.css'
 
 const Contact = () => {
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    const service_id = 'iaw_portafolio';
+    const template_id = 'template_gpwfeyp';
+    const user_id = 'D5cKw83dfCeB74Jma';
+
+    emailjs.sendForm(service_id, template_id, e.target, user_id)
+      .then((result) => {
+        console.log(result.text);
+        alert('Solicitud enviada con éxito');
+        e.target.reset();
+      })
+      .catch((error) => {
+        console.log(error.text);
+        alert('Error al enviar el correo. Inténtalo de nuevo más tarde.');
+      });
+  };
+
   return (
-    <article id='contactPage'>
-		<form>
-			<div class="contact-form-info">
-				<h1>Escríbenos</h1>
+    <article id='contactPage' className={styles.contactBox}>
+      <form onSubmit={sendEmail}>
+        <div className={styles.contactInfo}>
+          <h1>Envía tu mensaje</h1>
 
-				<div class="name">
-					<p>Nombre</p>
-					<input type="text" placeholder="Nombre" required/>
-				</div>
+          <div>
+            <p>Nombre</p>
+            <input className={styles.inputArea} type="text" name="name" placeholder="Nombre" required />
+          </div>
 
-				<div class="surnames">
-					<p>Apellidos</p>
-					<input type="text" placeholder="Apellidos" required/>
-				</div>
+          <div>
+            <p>Apellidos</p>
+            <input className={styles.inputArea} type="text" name="surnames" placeholder="Apellidos" required />
+          </div>
 
-                <div class="email">
-					<p>Correo electrónico</p>
-					<input type="email" placeholder="ejemplo@gmail.com" required/>
-				</div>
-                
-				<div class="message">
-					<p>Mensaje</p>
-					<textarea name="message" rows="5" cols="30" placeholder="Deja tu Mensaje..."></textarea>
-                </div>
-                <div class="send">
-				    <button><a href="#">Enviar</a></button>
-                </div>
-            </div>
-		</form>
+          <div>
+            <p>Correo electrónico</p>
+            <input className={styles.inputArea} type="email" name="email" placeholder="ejemplo@gmail.com" required />
+          </div>
+
+          <div>
+            <p>Mensaje</p>
+            <input className={styles.textMessage} name="message" placeholder="Deja tu Mensaje..."></input>
+          </div>
+
+          <div>
+            <button  className={styles.sendInfo} type="submit">Enviar</button>
+          </div>
+        </div>
+      </form>
     </article>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
